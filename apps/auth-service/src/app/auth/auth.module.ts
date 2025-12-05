abbff59ababbe5244ef4fee736f0b1041513d28e
bhashanasirimanna <bhashanasirimanna@gmail.com>
2025-12-05 12:49:25 +0530

    Add AuthModule with Mongoose integration and Kafka transport configuration

---
+++ apps/auth-service/src/app/auth/auth.module.ts
@@ -0,0 +1,42 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
+
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+// import { GoogleStrategy } from './strategies/google.strategy';
+// import { FacebookStrategy } from './strategies/facebook.strategy';
+import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'secret',
+      signOptions: { expiresIn: '7d' },
+    }),
+    ClientsModule.register([
+      {
+        name: 'EMAIL_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'auth-service',
+            brokers: [process.env.KAFKA_BROKER || 'localhost:29092'],
+          },
+          consumer: {
+            groupId: 'auth-consumer-group',
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
