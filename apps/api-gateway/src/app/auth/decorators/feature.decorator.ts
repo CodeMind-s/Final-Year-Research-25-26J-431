e46238588cb00d2bb8f74e35f82785b154f493fd
arshartisan <arshartisan@gmail.com>
2026-02-19 13:58:53 +0530

    feat(auth): remove deprecated feature endpoint and entitlement configurations

--- apps/api-gateway/src/app/auth/decorators/feature.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const FEATURE_KEY = 'requiredFeature';
-export const RequireFeature = (featureKey: string) =>
-  SetMetadata(FEATURE_KEY, featureKey);
