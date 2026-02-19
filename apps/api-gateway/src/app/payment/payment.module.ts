88165e796d619a072d5ed1fcc3be0c7d9e62f79b
arshartisan <arshartisan@gmail.com>
2026-02-19 14:42:51 +0530

    feat(payment): add PaymentModule and PaymentController for payment processing

---
+++ apps/api-gateway/src/app/payment/payment.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { GrpcExceptionFilter } from '../../filters/grpc-exception.filter';
+import { PaymentController } from './payment.controller';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'PAYMENT_PACKAGE',
+        transport: Transport.GRPC,
+        options: {
+          package: 'payment',
+          protoPath: join(__dirname, 'proto/payment.proto'),
+          url: process.env.PAYMENT_SERVICE_URL || 'localhost:50056',
+          loader: {
+            keepCase: true,
+            longs: String,
+            enums: String,
+            defaults: true,
+            oneofs: true,
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [PaymentController],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: GrpcExceptionFilter,
+    },
+  ],
+})
+export class PaymentModule {}
