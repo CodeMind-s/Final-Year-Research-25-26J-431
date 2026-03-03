2123435f7464bdfe42a7ad9da940961446fa75f9
arshartisan <arshartisan@gmail.com>
2026-03-03 20:49:01 +0530

    feat: add Jest configuration files for api-gateway, crystallization-onnx-service, and email-service

---
+++ apps/api-gateway/jest.config.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+const { readFileSync } = require('fs');
+const { join } = require('path');
+
+// Reading the SWC compilation config for the spec files
+const swcJestConfig = JSON.parse(
+  readFileSync(join(__dirname, '.spec.swcrc'), 'utf-8')
+);
+
+// Disable .swcrc look-up by SWC core because we're passing in swcJestConfig ourselves
+swcJestConfig.swcrc = false;
+
+module.exports = {
+  displayName: '@brinex-server/api-gateway',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['@swc/jest', swcJestConfig],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: 'test-output/jest/coverage',
+};
