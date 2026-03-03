26790fe8a7af4d7d46229917614b8b50bfa03839
Thimesha Ansar <101446688+thimeshaA@users.noreply.github.com>
2026-03-04 00:03:53 +0530

    feat: configure Mongoose module for MongoDB connection in app module

--- apps/crystallization-onnx-service/src/app/app.module.ts
+++ apps/crystallization-onnx-service/src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PredictionsModule } from './predictions/predictions.module';
 
 @Module({
-    imports: [PredictionsModule],
+    imports: [
+        MongooseModule.forRoot(
+            process.env.MONGO_URI ||
+            'mongodb+srv://brinexAdmin:1no83DWF6n31kkj3@cluster0.tk0ipzf.mongodb.net/brinex?appName=Cluster0',
+        ),
+        PredictionsModule,
+    ],
 })
-export class AppModule { }
+export class AppModule {}
